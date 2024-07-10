4183b2e94bb6db8b227354f497e5190149405002
twinarta <163377876+twinartadl@users.noreply.github.com>
2024-07-10 23:22:25 +0800

    Fix

--- app_demo_advanced.py
+++ app_demo_advanced.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 from langchain_openai import AzureOpenAIEmbeddings, AzureChatOpenAI
 from langchain.vectorstores import Chroma
